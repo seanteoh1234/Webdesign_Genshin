3daa6a54d7e7fe6251d4d0d02e04bf43f0a02162
seanteoh1234 <92786434+seanteoh1234@users.noreply.github.com>
2022-02-26 23:45:51 +0800

    MSI Laptop

---
+++ script.js
@@ -0,0 +1,6 @@
+var text1 = "Hi, my name is Sean and I am";
+var years = 23;
+var text2 = "years old.";
+var text3 = "Thanks for visiting my website :D"
+
+alert (text1 +" "+ years +" "+text2 +" "+ text3);
